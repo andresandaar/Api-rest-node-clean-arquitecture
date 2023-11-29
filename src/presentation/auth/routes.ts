51e78b8f761b87dde9c98dc7a8bcf0ffb55db89b
andresandaar <davilaarroyo07@gmail.com>
2023-11-28 23:06:00 -0500

    configurAR mongodb con docker

--- src/presentation/auth/routes.ts
+++ src/presentation/auth/routes.ts
@@ -1,12 +1,15 @@
 import { Router } from "express";
 import { AuthController } from "./controller";
+import { AuthRepositoryImpl, AuthjDatasourceImpl } from "../../infrastructure";
 
 export class AuthRautes {
 
 static get  routes():Router {
 
 const router =Router();
-const controller = new AuthController();
+const datasource=new AuthjDatasourceImpl()
+const authRepository=new AuthRepositoryImpl(datasource)
+const controller = new AuthController(authRepository);
 
 //Definir todas las rutas principales
 router.post('/login',controller.loginUser);
